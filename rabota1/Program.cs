042e6851a5884004052856ce3d6fab404fe2b8b4
Artasel <aleksejbobkin163@gmail.com>
2022-07-12 13:05:21 +0200

    домашка

---
+++ rabota1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+string res = "";
+int count = 0;
+
+while (true)
+{
+    Console.WriteLine("Введите числа");
+
+    string? input = Console.ReadLine();     //вопрос означает готовность принять null вроде как
+
+    bool provChtoChisloWwedeno = int.TryParse(input, out int result);  //проверка на число
+
+    if(provChtoChisloWwedeno)
+    {
+        if(res == "")  res += input;
+        else    res += "," + input;
+        count++;
+    }
+    if(!provChtoChisloWwedeno)  break;
+}
+
+
+int[] Mass = new int[count];
+string sborochnaiaPeremen = "";
+int convertPeremen = 0;
+int count2 = 0;
+
+for(int i = 0; i < res.Length; i++)
+{
+    bool provChtoChislo = Char.IsNumber(res[i]);  //проверка на число
+
+    if(provChtoChislo || res[i] == '-')
+    {
+        sborochnaiaPeremen += res[i];
+    }  
+    else
+    {
+        convertPeremen = Convert.ToInt32(sborochnaiaPeremen);
+        Mass[count2] = convertPeremen;
+        count2++;
+        sborochnaiaPeremen = "";
+    }
+    if(i == res.Length - 1)
+    {
+        convertPeremen = Convert.ToInt32(sborochnaiaPeremen);
+        Mass[count2] = convertPeremen;
+    }
+}
+
+Console.WriteLine($"{String.Join(", ", Mass)}");
+
+int bolsheNolia = 0;
+
+for(int i = 0; i < Mass.Length; i++)
+{
+    if(Mass[i] > 0) bolsheNolia++;
+}
+
+Console.WriteLine($"Пользователь ввел {bolsheNolia} чисел больше ноля.");
---
+++ rabota2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*в общем, приравниваем правые части данных равенств:
+k1*x+b1=k2*x+b2
+выразим отсюда x:
+x=(b2-b1)/(k1-k2)
+затем подставим полученное значение х в выражение для у (для любого, например первого) :
+y=k1(b2-b1)/(k1-k2)+b1
+ответ: x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1
+*/
+
+Console.WriteLine("Введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+
+double y = 5 * (4 - 2) / (5 - 9) + 2;          //выполняются по разному и имеют разный результат. странно, ведь это одно и тоже
+double y2 = k1 * (b2 - b1) / (k1 - k2) + b1;   //выполняются по разному и имеют разный результат. странно, ведь это одно и тоже
+
+Console.WriteLine($"y равен = {y}");
+Console.WriteLine($"({x}; {y2})");
